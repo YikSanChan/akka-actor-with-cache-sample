e0265136ec51239dae6659e560583f4db3c376d2
Evan Chan <echan@tubi.tv>
2020-02-14 22:22:33 -0800

    Separating Concerns

--- src/main/scala/com/example/AkkaQuickstart.scala
+++ src/main/scala/com/example/AkkaQuickstart.scala
@@ -1,10 +1,9 @@
 package com.example
 
 
-import akka.actor
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
-import com.example.Cache.CacheRequests
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, SupervisorStrategy}
+import com.example.Cache.{CacheRequests, Devices}
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContextExecutor, Future}
@@ -14,43 +13,58 @@ case class RequestId(s: String)
 
 case class DeviceId(s: String)
 
-object Cache {
+object Timer {
+  private case object TimerKey
 
+  sealed trait TimerMessage
+  private case object Tick extends TimerMessage
+  private case class FailedRequest(ex: Throwable) extends TimerMessage
+  private case class SuccessfulRequest(devices: List[DeviceId]) extends TimerMessage
+
+  def start(cache: ActorRef[CacheRequests], request: => Future[List[DeviceId]]): Behavior[TimerMessage] =
+    Behaviors.withTimers { timer =>
+      timer.startPeriodicTimer(TimerKey, Tick, 5.seconds)
+      Behaviors.receive[TimerMessage] { (context, message) =>
+        message match {
+          case Tick =>
+            context.log.info("Executing request.")
+            implicit val ec: ExecutionContextExecutor = context.executionContext
+            request.onComplete {
+              case Success(devices) => context.self ! SuccessfulRequest(devices)
+              case Failure(ex)      => context.self ! FailedRequest(ex)
+            }
+            Behaviors.same
+          case SuccessfulRequest(devices) =>
+            context.log.info("Successful response.")
+            cache ! Devices(devices)
+            Behaviors.same
+          case FailedRequest(ex) =>
+            context.log.error("Failed response.")
+            throw ex
+        }
+      }
+    }
+}
+
+object Cache {
   sealed trait CacheRequests
   final case class Get(requestId: RequestId, replyTo: ActorRef[CacheResponses]) extends CacheRequests
-  final case class CachedDevices(devices: List[DeviceId]) extends CacheResponses
-  final private case class Devices(devices: List[DeviceId]) extends CacheRequests
+  final case class Devices(devices: List[DeviceId]) extends CacheRequests
 
   sealed trait CacheResponses
   final case object EmptyCache extends CacheResponses
-  final private case object Tick extends CacheRequests
-  final private case object CacheRefreshFailed extends CacheRequests
-
-  private case object TimerKey
+  final case class CachedDevices(devices: List[DeviceId]) extends CacheResponses
 
   val empty: Behavior[CacheRequests] =
-    Behaviors.withTimers { timer =>
-      timer.startPeriodicTimer(TimerKey, Tick, 5.second)
-
-      Behaviors.setup { context =>
-        refreshCache(context.self, context.system)
-
-        Behaviors.receiveMessage[CacheRequests] {
-          case Get(requestId, replyTo) =>
-            context.log.info("Empty cache request for requestId {}.", requestId)
-            replyTo ! EmptyCache
-            Behaviors.same
-          case Devices(devices) =>
-            context.log.info("Initialized cache.")
-            cached(devices)
-          case CacheRefreshFailed =>
-            context.log.error("Failed to initialize cache. Will retry.")
-            Behaviors.same
-          case Tick =>
-            context.log.info("Refreshing cache.")
-            refreshCache(context.self, context.system)
-            Behaviors.same
-        }
+    Behaviors.receive[CacheRequests] { (context, message) =>
+      message match {
+        case Get(requestId, replyTo) =>
+          context.log.info("Empty cache request for requestId {}.", requestId)
+          replyTo ! EmptyCache
+          Behaviors.same
+        case Devices(devices) =>
+          context.log.info("Initializing cache.")
+          cached(devices)
       }
     }
 
@@ -61,34 +75,36 @@ object Cache {
           context.log.info("Cache request for requestId {}.", requestId)
           replyTo ! CachedDevices(devices)
           Behaviors.same
-        case Devices(devices) =>
-          context.log.info("Updated cache.")
-          cached(devices)
-        case CacheRefreshFailed =>
-          context.log.error("Failed to refresh cache.")
-          Behaviors.same
-        case Tick =>
-          context.log.info("Refreshing cache.")
-          refreshCache(context.self, context.system)
-          Behaviors.same
+        case Devices(updatedDevices) =>
+          context.log.info("Updating cache.")
+          cached(updatedDevices)
       }
     }
+}
 
-  private def refreshCache(self: ActorRef[CacheRequests], system: ActorSystem[Nothing]): Unit = {
-    import akka.actor.typed.scaladsl.adapter._
-    implicit val untypedSystem: actor.ActorSystem = system.toClassic
-    implicit val ec: ExecutionContextExecutor = untypedSystem.dispatcher
+object Main {
+  case class Ping()
 
+  def apply(): Behavior[Ping] = Behaviors.setup {context =>
     val random = new Random()
-
-    // From a device source
-    Future.successful(List.fill(100)(random.nextInt(100).toString).map(DeviceId)).onComplete {
-      case Success(devices) => self ! Devices(devices)
-      case Failure(_) => self ! CacheRefreshFailed
+    def getDevices: Future[List[DeviceId]] =
+      Future.successful(List.fill(100)(random.nextInt(100).toString).map(DeviceId))
+    val cache = context.spawn(Cache.empty, "cache")
+    val timer = Behaviors
+    .supervise {
+      Timer.start(cache, getDevices)
     }
+    .onFailure[Throwable] {
+      SupervisorStrategy.restartWithBackoff(
+        minBackoff = 0.seconds,
+        maxBackoff = 60.seconds,
+        randomFactor = 0.1)
+    }
+    context.spawn(timer, "timer")
+    Behaviors.same
   }
 }
 
 object SimpleCache extends App {
-  val cache = ActorSystem[CacheRequests](Cache.empty, "cache")
+  val main: ActorSystem[Main.Ping] = ActorSystem(Main(), "main")
 }
